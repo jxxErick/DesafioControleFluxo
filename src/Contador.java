89cf43fe3d5ccbe1348b9e4bf6400374c1e0f6ac
jxxErick <ericksdantas144@gmail.com>
2023-10-21 19:13:27 -0300

    Conclusao do desafio

---
+++ src/Contador.java
@@ -0,0 +1,33 @@
+import exception.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            if (i < parametroDois){ System.out.print(i + ", ");
+            } else {System.out.print(i + ".");
+
+            }
+        }
+    }
+}
+
